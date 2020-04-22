847ab85c0e40e5aaa32178fc69dfdeb041b7214e
Ben Robinson <ben@joinfoodchain.com>
2020-04-23 00:46:05 +0100

    Add type-aware lints

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,16 @@
 module.exports = {
     root: true,
     parser: "@typescript-eslint/parser",
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ["./tsconfig.json"],
+    },
     plugins: ["@typescript-eslint", "react"],
     extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
         "plugin:react-hooks/recommended",
         "plugin:react/recommended",
         "prettier",
@@ -14,7 +19,12 @@ module.exports = {
         "@typescript-eslint/explicit-function-return-type": 0,
         "@typescript-eslint/member-delimiter-style": 0,
         "@typescript-eslint/camelcase": 0,
+        "@typescript-eslint/no-inferrable-types": 0,
+        "@typescript-eslint/strict-boolean-expressions": 2,
+        "@typescript-eslint/switch-exhaustiveness-check": 2,
+        "@typescript-eslint/restrict-plus-operands": 2,
         "react/prop-types": 0,
+        "no-empty": 0,
     },
     settings: {
         react: {
