89b605ccad3d5699103392775c01f82dd35c72e3
Ben Robinson <ben@joinfoodchain.com>
2020-04-25 11:20:21 +0100

    Fix cookie notice UI
    Still needs to tie into GA

--- src/components/cookie-notice.tsx
+++ src/components/cookie-notice.tsx
@@ -1,5 +1,8 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { graphql, useStaticQuery } from "gatsby"
+import Cookies from "js-cookie"
+
+const COOKIE_CONSENT_KEY = "ccm_cookie_consent"
 
 const CookieNotice = () => {
     const data = useStaticQuery<GatsbyTypes.CookieNoticeQuery>(graphql`
@@ -11,15 +14,49 @@ const CookieNotice = () => {
             }
         }
     `)
+
+    const [consentResponse, setConsentResponse] = useState<string | undefined>(
+        () => Cookies.get(COOKIE_CONSENT_KEY)
+    )
+
+    useEffect(() => {
+        if (consentResponse != null) {
+            Cookies.set(COOKIE_CONSENT_KEY, consentResponse, { expires: 365 })
+        }
+        if (consentResponse === "accepted") {
+            // Enable GA
+        } else {
+            // Disable GA
+        }
+    }, [consentResponse])
+
     return (
-        <div id="cookie-notice" className="cookie-notice">
+        <div
+            id="cookie-notice"
+            className="cookie-notice"
+            style={{ bottom: consentResponse != null ? undefined : 0 }}
+        >
             <div className="notice">
                 {data?.site?.siteMetadata?.cookie_notice}
             </div>
-            <a id="decline-cookie-button" className="button decline" href="#">
+            <a
+                id="decline-cookie-button"
+                className="button decline"
+                href="#"
+                onClick={() => {
+                    setConsentResponse("declined")
+                }}
+            >
                 Decline
             </a>
-            <a id="accept-cookie-button" className="button accept" href="#">
+            <a
+                id="accept-cookie-button"
+                className="button accept"
+                href="#"
+                onClick={() => {
+                    setConsentResponse("accepted")
+                }}
+            >
                 Accept
             </a>
         </div>
@@ -27,53 +64,3 @@ const CookieNotice = () => {
 }
 
 export default CookieNotice
-
-// function hideCookieNotice() {
-//     var cookie_notice = document.getElementById("cookie-notice");
-
-//     setTimeout(function() {
-//         cookie_notice.style.bottom = "-400px";
-//     },200);
-
-//     setTimeout(function() {
-//         cookie_notice.style.display = "none";
-//     },1000);
-// }
-
-// function acceptCookies() {
-//     console.log("Accepted cookies!");
-//     hideCookieNotice();
-//     Cookies.set('ccm_cookie_consent', 'accepted', { expires: 365 });
-// }
-
-// function declineCookies() {
-//     console.log("Declined cookies!");
-//     hideCookieNotice();
-//     Cookies.set('ccm_cookie_consent', 'declined', { expires: 365 });
-// }
-
-// function disableGATracking() {
-//     window['ga-disable-UA-39016872-2'] = true;
-// }
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     var cookie_consent = Cookies.get('ccm_cookie_consent')
-
-//     var accept_cookie_button = document.getElementById("accept-cookie-button");
-//     var decline_cookie_button = document.getElementById("decline-cookie-button");
-
-//     accept_cookie_button.addEventListener("click",acceptCookies);
-//     decline_cookie_button.addEventListener("click",declineCookies);
-
-//     if (cookie_consent == null) {
-//         console.log("User has not seen or consented to cookies");
-
-//         var cookie_notice = document.getElementById("cookie-notice");
-
-//         setTimeout(function() {
-//             cookie_notice.style.bottom = "0px";
-//         },2000);
-//     } else if (cookie_consent == "accepted") {
-//         disableGATracking();
-//     }
-// });
