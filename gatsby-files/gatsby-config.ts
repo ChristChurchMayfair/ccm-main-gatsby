fdf3e14dfcfe0e73b9df7d729d6c08ccce5e698a
Ben Robinson <ben@txrx.org.uk>
2020-05-26 19:21:46 +0100

    Links from markdown now use client-side push

--- gatsby-files/gatsby-config.ts
+++ gatsby-files/gatsby-config.ts
@@ -126,6 +126,7 @@ export const plugins: GatsbyConfig["plugins"] = [
             },
         },
     },
+    "gatsby-plugin-catch-links",
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
