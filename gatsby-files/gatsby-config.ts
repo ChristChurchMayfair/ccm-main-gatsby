8f49c766b9852172236bd2d1c00862c9aaeba56b
Tom Duckering <tduckering@apple.com>
2020-06-01 21:16:11 +0100

    Add Percy config
    With an API token set, this plugin will send screenshots to a service to
    allow us to compare one build to another visually.

--- gatsby-files/gatsby-config.ts
+++ gatsby-files/gatsby-config.ts
@@ -131,4 +131,5 @@ export const plugins: GatsbyConfig["plugins"] = [
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    `gatsby-plugin-percy`
 ]
