d4d0f4736692f0c950b96d7be03b2f2d42fe3eda
Tom Duckering <tduckering@apple.com>
2021-01-08 13:14:01 +0000

    Adding congregational survey
    Involves adding some missing features to the forms stuff.

--- src/components/form/fields/form-section-start.tsx
+++ src/components/form/fields/form-section-start.tsx
@@ -1,6 +1,22 @@
 import React from "react"
+import { shouldShowField, VisibilityCondition } from "../conditional-visibility"
+import { ReactHookFormWiring } from "../form-config.types"
 import formStyles from "../form.module.scss"
 
-export const FormInformationSection: React.FC = ({ children }) => {
+type Props = {
+    showWhen?: VisibilityCondition
+} & ReactHookFormWiring
+
+export const FormInformationSection: React.FC<Props> = ({
+    showWhen,
+    watch,
+    children,
+}) => {
+    const shouldDisplayThisField = shouldShowField(showWhen, watch)
+
+    if (shouldDisplayThisField === false) {
+        return null
+    }
+
     return <div className={formStyles.sectionInfo}>{children}</div>
 }
