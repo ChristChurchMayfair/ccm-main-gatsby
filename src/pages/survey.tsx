44ffef2f99d8fc9b7da3b884b0b58e04097e9786
Tom Duckering <tduckering@apple.com>
2021-01-24 12:09:19 +0000

    Fix formatting

--- src/pages/survey.tsx
+++ src/pages/survey.tsx
@@ -243,7 +243,7 @@ const SurveyPage: React.FC = () => {
                     name="ageBracket"
                     label="Indicate your age bracket"
                     contextualHelp={"Select one."}
-                    validation={{required: "This is a required field"}}
+                    validation={{ required: "This is a required field" }}
                     options={[
                         { id: "underTwelve", name: "Under 12" },
                         { id: "twelveToSeventeen", name: "12 to 17" },
@@ -267,7 +267,7 @@ const SurveyPage: React.FC = () => {
                     name="areYouAStudent"
                     label="Are you a student?"
                     contextualHelp={"Select one."}
-                    validation={{required: "This is a required field"}}
+                    validation={{ required: "This is a required field" }}
                     options={[
                         { id: "student", name: "Student" },
                         {
@@ -281,7 +281,7 @@ const SurveyPage: React.FC = () => {
                     name="gender"
                     label="Gender"
                     contextualHelp={"Select one."}
-                    validation={{required: "This is a required field"}}
+                    validation={{ required: "This is a required field" }}
                     options={[
                         { id: "male", name: "Male" },
                         { id: "female", name: "Female" },
@@ -295,7 +295,7 @@ const SurveyPage: React.FC = () => {
                     name="yourFirstLanguage"
                     label="Please tell us your first language"
                     contextualHelp={"Select one."}
-                    validation={{required: "This is a required field"}}
+                    validation={{ required: "This is a required field" }}
                     options={[
                         { id: "english", name: "English", checked: true },
                     ]}
@@ -307,7 +307,7 @@ const SurveyPage: React.FC = () => {
                     name="yourLocation"
                     label="Please tell us where you live"
                     contextualHelp={"City and/or Country."}
-                    validation={{required: "This is a required field"}}
+                    validation={{ required: "This is a required field" }}
                     options={[
                         {
                             id: "london",
@@ -330,7 +330,7 @@ const SurveyPage: React.FC = () => {
                     name="serviceAttended"
                     label="Which service time are you attending now?"
                     contextualHelp={"Select one."}
-                    validation={{required: "This is a required field"}}
+                    validation={{ required: "This is a required field" }}
                     options={[
                         { id: "morning", name: "Morning (AM)" },
                         { id: "evening", name: "Evening (PM)" },
@@ -340,7 +340,7 @@ const SurveyPage: React.FC = () => {
                     name="liveOrCatchUp"
                     label="How did you attend today?"
                     contextualHelp={"Select one."}
-                    validation={{required: "This is a required field"}}
+                    validation={{ required: "This is a required field" }}
                     options={[
                         { id: "inPerson", name: "In Person" },
                         { id: "liveStreamed", name: "Online - Live" },
@@ -351,7 +351,7 @@ const SurveyPage: React.FC = () => {
                     name="alsoWatchedMorningService"
                     label="Did you also attend this morning's service?"
                     contextualHelp={"Select one."}
-                    validation={{required: "This is a required field"}}
+                    validation={{ required: "This is a required field" }}
                     options={[
                         { id: "yesInPerson", name: "Yes - In Person" },
                         { id: "yesLiveStreamed", name: "Yes - Live Streamed" },
@@ -363,7 +363,7 @@ const SurveyPage: React.FC = () => {
                     name="regularOrVisitor"
                     label="Are you a regular attender or just visiting?"
                     contextualHelp={"Select one."}
-                    validation={{required: "This is a required field"}}
+                    validation={{ required: "This is a required field" }}
                     options={[
                         { id: "regular", name: "Regular" },
                         { id: "visitor", name: "Visitor" },
@@ -373,7 +373,7 @@ const SurveyPage: React.FC = () => {
                     name="howLongHaveYouBeenAttending"
                     label="How long have you been attending Christ Church Mayfair?"
                     contextualHelp={"Select one."}
-                    validation={{required: "This is a required field"}}
+                    validation={{ required: "This is a required field" }}
                     showWhen={showRegularAttenderQuestions}
                     options={[
                         { id: "lessThanAYear", name: "Less than a year" },
@@ -391,7 +391,7 @@ const SurveyPage: React.FC = () => {
                     name="howDidYouFindThisService"
                     label="How did you discover Christ Church Mayfair?"
                     contextualHelp={"Optional."}
-                    validation={{required: "This is a required field"}}
+                    validation={{ required: "This is a required field" }}
                     allowOther={true}
                     otherInputLabel="Another Way"
                     otherOptionName="Another Way"
@@ -420,7 +420,7 @@ const SurveyPage: React.FC = () => {
                     contextualHelp={
                         "Select one. DG includes DG central, Student DG, DG Daytime, DG@Home"
                     }
-                    validation={{required: "This is a required field"}}
+                    validation={{ required: "This is a required field" }}
                     options={[
                         {
                             id: "dg",
