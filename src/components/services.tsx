7bb09e162ba06ee0bb97017a43cdfbe5b0284d89
PeterBalsdon <75443454+PeterBalsdon@users.noreply.github.com>
2021-10-01 09:54:03 +0100

    Update services.tsx

--- src/components/services.tsx
+++ src/components/services.tsx
@@ -49,10 +49,10 @@ const Services = () => {
             <div className={styles.servicesSection}>
                 <h1 className={styles.heading}>Our Sunday Services</h1>
                 <div className={styles.serviceNotes}>
-					Our services have now mostly returned to normal. We ask that
-					you please wear a mask when singing, and we are still
-					keeping an area of the church with a bit more space.
-				</div>
+                    Our services have now mostly returned to normal. We ask that
+                    you please wear a mask when singing, and we are still
+                    keeping an area of the church with a bit more space.
+                </div>
                 <div className={styles.services}>
                     {[data.am, data.pm].map(service => {
                         if (service == null) {
